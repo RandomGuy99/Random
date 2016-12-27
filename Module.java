119f6e7173ac027a7bd3bf33e069661c5e8f6a4b
RandomGuy99 <Kadiyala.java@gmail.com>
2016-12-27 10:54:14 -0500

    Add files via upload

---
+++ Module.java
@@ -0,0 +1,22 @@
+package com.verizon.delphi.model;
+
+public class Module {
+	private String id;
+    private String type;
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+	
+}
---
+++ Port.java
@@ -0,0 +1,21 @@
+package com.verizon.delphi.model;
+
+public class Port {
+ private String id;
+ private String type;
+public String getType() {
+	return type;
+}
+
+public void setType(String type) {
+	this.type = type;
+}
+
+public String getId() {
+	return id;
+}
+
+public void setId(String id) {
+	this.id = id;
+}
+}
